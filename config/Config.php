ffa8567daa78326aac5d3194091cfddcb975580a
jocscriptch <jocsanchaves20@gmail.com>
2023-08-21 10:52:59 -0600

    gitignore

--- config/Config.php
+++ config/Config.php
@@ -6,5 +6,5 @@ define('USER', 'root');
 define('PASS', '');
 define('DBNAME', 'sistema');
 define('CHARSET', 'utf8mb4');
-define('TITLE', 'CARDENAL POS')
+define('TITLE', 'MOTO RESPUESTOS CARDENAL')
 ?>
