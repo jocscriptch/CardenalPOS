51ec83534483ec01a7f69f7a85e2d2eac1a88a40
jocscriptch <jocsanchaves20@gmail.com>
2024-04-21 19:51:13 -0600

    config-aiven

--- config/Config.php
+++ config/Config.php
@@ -1,11 +1,12 @@
 <?php
 define('BASE_URL', 'http://localhost/sistema/');
-define('HOST', 'localhost');
-define('PORT', 3306);
-define('USER', 'root');
-define('PASS', '');
-define('DBNAME', 'cardenalpos');
+define('HOST', 'mysql-8acc441-cardenalpos-ab72.b.aivencloud.com');
+define('PORT', 28452);
+define('USER', 'avnadmin');
+define('PASS', 'AVNS_QK4XTboHhfq_PgOcBKx');
+define('DBNAME', 'dbcardenalpos');
 define('CHARSET', 'utf8mb4');
+define('SSL', 'REQUIRED');
 define('TITLE', 'CARDENAL POS');
 define('CLAVE_SMTP', 'caicnksgnokfhlur');
 define('HOST_SMTP', 'smtp.gmail.com');
